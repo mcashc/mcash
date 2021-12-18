3c1e2e0d686210a3fc17a9c2e9390f7d1510e514
Ansarul Mullah <mdansarul543@gmail.com>
2021-12-18 19:38:30 +0600

    O

--- src/rpcclient.py
+++ src/rpcclient.py
@@ -5,8 +5,8 @@ hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 port=9091
 def main():
-    conn = Server('http://'+local_ip+":"+str(port))
-    #conn = Server('http://localhost:9091')
+    #conn = Server('http://'+local_ip+":"+str(port))
+    conn = Server('http://161.97.89.98:9091')
     #block index 8487
     #print(conn.chainStatus())
     #print(conn.resolve(),"\n")
@@ -20,7 +20,7 @@ def main():
     #print(conn.addTransaction("fbf0b8ebfcc1876ddf9e9fbecb084eda4f466070a49a0f7198702549127ff9dc","0x779dc4d50681bbeb2b4c46c2410c94e684d70c66",952267.00000000))
     #print(conn.pendingTrx())
     while True:
-        print(conn.mining("M Miner","0x16c243a961f56819688e2cefeba9a268ce40abd0"))
+        print(conn.mining("M Miner","0xeee76f840a6fAd52F4dcd8d24406cc77F2B6b695"))
     #print(conn.pendingTrx())
 	#0xeee76f840a6fAd52F4dcd8d24406cc77F2B6b695
     print(conn.lastblock())
